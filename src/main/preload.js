0c6a2eb07028c98bd06ff193ff7813970df41c34
Lee Hom <geekeea@gmail.com>
2025-10-27 15:14:58 +0800

    feat: add electron vue tool for script search and execution

---
+++ src/main/preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  selectDirectory: () => ipcRenderer.invoke('dialog:selectDirectory'),
+  searchFiles: (payload) => ipcRenderer.invoke('search:files', payload),
+  listScripts: (directory) => ipcRenderer.invoke('scripts:list', directory),
+  executeScripts: (payload) => ipcRenderer.invoke('scripts:execute', payload),
+  openFile: (filePath) => ipcRenderer.invoke('path:open', filePath),
+  revealFile: (filePath) => ipcRenderer.invoke('path:reveal', filePath)
+});
